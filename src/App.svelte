c0ca2823645717ff105c13f2db55ae364f9cb3ad
yfurmanau <yfurmanau@gmail.com>
2024-06-13 19:59:18 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<img {src} alt="Random picture" />
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>{@html string}</p>
+
+<script>
+  let name = "Svelte";
+  let src = "https://picsum.photos/200/200";
+  let string = "Some <em>string</em> with html";
+</script>
+
+<style>
+  h1 {
+    color: blueviolet;
+  }
+</style>
