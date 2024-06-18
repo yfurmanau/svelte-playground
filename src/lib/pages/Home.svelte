bc11d50d482e3ead50f2b5361bb77a8f5a226fc4
yfurmanau <yfurmanau@gmail.com>
2024-06-18 15:49:43 +0200

    use twened store

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,6 +1,14 @@
 <script>
   import settings from "../stores/settings.js";
   import location from "../stores/location.js";
+
+  import { tweened } from "svelte/motion";
+  import { cubicOut } from "svelte/easing";
+
+  const scale = tweened(1, {
+    duration: 200,
+    easing: cubicOut,
+  });
 </script>
 
 <h2>Home</h2>
@@ -10,3 +18,9 @@
   {$location.longitude}
 {/if}
 {$settings.colorScheme}
+
+<button on:click={() => ($scale += 1)}>Make it bigger</button>
+<button on:click={() => ($scale -= 1)}>Make it smaller</button>
+<div
+  style="width: 100px; height: 100px; background-color: purple; transform: scale({$scale}); transform-origin: 0 0;"
+/>
