896a39df00e94ffbb52c057ee4fc0fdfa45eb0c5
yfurmanau <yfurmanau@gmail.com>
2024-07-25 19:58:28 +0200

    add adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import { sveltePreprocess } from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 
 const config = {
 	preprocess: sveltePreprocess({
 		scss: {}
-	})
+	}),
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
